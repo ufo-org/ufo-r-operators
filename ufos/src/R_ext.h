afbb631096f0a1d1e8c64d8b79deefefb5cb7698
Konrad Siek <konrad.siek@gmail.com>
2021-09-16 17:18:13 +0200

    R UFO vector implementations.

--- ufos/src/R_ext.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define USE_RINTERNALS
-
-#include <R.h>
-#include <Rinternals.h>
-#include <R_ext/Rallocators.h>
-
-SEXP allocMatrix3(SEXPTYPE mode, int nrow, int ncol, R_allocator_t *allocator);
